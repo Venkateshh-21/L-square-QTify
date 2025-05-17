38df4f52fda14507819a1544b37e746694a9e4f7
venkatesh-21 <rajavenkatesh610@gmail.com>
2025-05-17 21:18:23 +0530

    added new albums without padding top

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/navBar';
 import Hero from './components/herosection';
 import Albums from "./components/albums"
+import NewAlbums from './components/newAlbums';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Albums />
+      <NewAlbums />
     </div>
   );
 }
---
+++ qtify/src/components/newAlbums.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import axios from "axios";
+import { Stack } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import "./newAlbums.css"
+
+export default function NewAlbums() {
+  const [TopAlbums, setTopAlbums] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("https://qtify-backend-labs.crio.do/albums/new");
+      setTopAlbums(response.data);
+    };
+    fetchData();
+  }, []);
+  console.log(TopAlbums);
+  return (
+    <div className="topAlbums">
+    
+      <div  className="albums"style={{ paddingLeft: "50px", paddingRight: "50px",paddingBottom:"50px" }}>
+        <Typography variant="h5" component="h4" className="heading">
+          Top Albums
+        </Typography>
+       
+        <Button id="collapse">Collapse</Button>
+      </div>
+      <Grid className="grid"
+        style={{ paddingLeft: "50px" }}
+        container
+        rowSpacing={4}
+        columnSpacing={{ xs: 1, sm: 2, md: 5, lg: 7,xl:12 }}
+      >
+        {TopAlbums.map((a) => {
+          return (
+            <>
+              <Grid className="gridItem">
+                <Stack direction={"column"} key={a.id}>
+                  <Card sx={{ maxWidth: 200, maxHeight: 232 }}>
+                    <CardMedia
+                      className="image"
+                      sx={{ height: 140 }}
+                      image={a.image}
+                      style={{ width: "159px", height: "179px" }}
+                      title={a.title+" "+a.songs.length+" Songs"}
+                    />
+                    <CardContent>
+                      <Chip
+                        label={a.follows + " Follows"}
+                        style={{ backgroundColor: "#121212", color: "#FFFFFF" }}
+                      />
+                      <div></div>
+                    </CardContent>
+                  </Card>
+                </Stack>
+                <Typography
+                  variant="p"
+                  component="h3"
+                  style={{ width: "159px",paddingTop:"20px"}}
+                >
+                  {a.title}
+                </Typography>
+              </Grid>
+            </>
+          );
+        })}
+      </Grid>
+    </div>
+  );
+}
